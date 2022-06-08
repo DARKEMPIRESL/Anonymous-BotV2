6980c4943034b7f9f3728542079e4311391c6075
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-06-08 08:12:37 +0530

    `Anonymous-BotV2`

--- AnonymousSenderBot/stats.py
+++ AnonymousSenderBot/stats.py
@@ -1,53 +1,24 @@
-import platform
-import shutil
-import psutil
-
+from AnonymousSenderBot.database.users_sql import Users, num_users
+from AnonymousSenderBot.database.chats_sql import num_chats
+from AnonymousSenderBot.database import SESSION
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
-from core import __version__ as bot_version
-from sys import version
-from pyrogram import __version__ as pyrogram_version
-from pytgcalls.__version__ import __version__ as pytgcalls_version
-from database.chat_database import ChatDB
-from database.lang_utils import get_message as gm
-from functions.stats_utils import humanbytes
+from config import configs
 
 
-@Client.on_message(filters.command("stats"))
-async def get_stats_(client: Client, m: Message):
-    bot_name = (await client.get_me()).first_name
-    python_version = version.split(" ")[0]
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = f"{psutil.cpu_percent()} %"
-    ram_usage = f"{psutil.virtual_memory().percent} %"
-    pm, chats = ChatDB().get_stats()
-    system_platform = platform.system()
-    architecture_machine = platform.machine()
-    chat_id = m.chat.id
-    text = gm(chat_id, "stats_text").format(
-        bot_name,
-        bot_version,
-        pytgcalls_version,
-        pyrogram_version,
-        python_version,
-        chats,
-        pm,
-        cpu_usage,
-        ram_usage,
-        used,
-        free,
-        total,
-        architecture_machine,
-        system_platform
-    )
-    await m.reply(text, disable_web_page_preview=True)
+@Client.on_message(~filters.edited & ~filters.service, group=1)
+async def users_sql(_, msg: Message):
+    if msg.from_user:
+        q = SESSION.query(Users).get(int(msg.from_user.id))
+        if not q:
+            SESSION.add(Users(msg.from_user.id))
+            SESSION.commit()
+        else:
+            SESSION.close()
 
 
-__cmds__ = ["stats"]
-__help__ = {
-    "stats": "help_stats"
-}
+@Client.on_message(filters.user(configs.OWNER_ID) & ~filters.edited & filters.command("stats"))
+async def _stats(_, msg: Message):
+    users = await num_users()
+    chats = await num_chats()
+    await msg.reply(f"Total Users : {users} \n\nTotal Chats : {chats}", quote=True)
