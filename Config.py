b134f1bfa206375433bc658e0a6208307b4b7e97
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-06-15 08:31:47 +0530

    `Anonymous-BotV2`

--- Config.py
+++ Config.py
@@ -1,21 +1,24 @@
 import os
 
-from dotenv import find_dotenv, load_dotenv
-load_dotenv(find_dotenv())
+ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
-
-class Config:
-
-    API_ID = int(os.environ.get('API_ID', 0))
+if ENVIRONMENT:
+    try:
+        API_ID = int(os.environ.get('API_ID', 0))
+    except ValueError:
+        raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    OWNER_ID = os.environ.get('OWNER_ID', None)
-    REDIS_URI = os.getenv("REDIS_URI", None)
-    REDIS_PASS = os.getenv("REDIS_PASS", None)
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
 
-
-
-
+else:
+    # Fill the Values
+    API_ID = 0
+    API_HASH = ""
+    BOT_TOKEN = ""
+    DATABASE_URL = ""
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
 
 DEVS = [1120271521]
-
