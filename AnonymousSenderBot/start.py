4ede1b9deecc2b87d29a7b0f35d580c9a9ff8d8c
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-06-16 08:04:10 +0530

    `Anonymous-BotV2`

--- AnonymousSenderBot/start.py
+++ AnonymousSenderBot/start.py
@@ -7,8 +7,6 @@ from pyrogram.types import  InlineKeyboardMarkup, InlineKeyboardButton, InputMed
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(anonbot, msg):
     print("/start")
-    user = await anonbot.get_me()
-    mention = user["mention"]
     await anonbot.send_message(
         msg.chat.id,
         Data.START,
