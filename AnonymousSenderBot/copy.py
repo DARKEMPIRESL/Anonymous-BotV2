98a59b8e73368d279e9918c16d00f2145f4eda55
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-06-21 08:15:10 +0530

    `Anonymous-BotV2`

--- AnonymousSenderBot/copy.py
+++ AnonymousSenderBot/copy.py
@@ -9,6 +9,6 @@ from pyrogram.types import InlineKeyboardMarkup
 )
 async def copy(_, msg):
     if msg.caption:
-        await msg.copy(msg.chat.id, reply_markup=InlineKeyboardMarkup([Data.remove_button]), disable_notification=True, reply_to_message_id=msg.message_id)
+        await msg.copy(msg.chat.id, reply_markup=InlineKeyboardMarkup([Data.add_button,Data.remove_button]), disable_notification=True, reply_to_message_id=msg.message_id)
     else:
-        await msg.copy(msg.chat.id)
+        await msg.copy(msg.chat.id, reply_markup=([Data.add_button]))
