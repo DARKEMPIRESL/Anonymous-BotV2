97bd117214a5dc699759403e0a642189c77f2674
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-06-27 16:37:58 +0530

    `Anonymous-BotV2`

--- Config.py
+++ Config.py
@@ -10,7 +10,7 @@ if ENVIRONMENT:
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
 
 else:
